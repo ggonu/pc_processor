c11900ef8e9295c1e2e1c33d21aecc1eb69728a1
ggonu <gogun0928@naver.com>
2024-10-23 14:41:41 +0900

    change undistortion method

--- src/pc_processor.cpp
+++ src/pc_processor.cpp
@@ -12,12 +12,12 @@
 
 // --- Define Macros --- //
                         // Uncomment Macro to Activate to using Voxel Grid Filter
-#define VOXEL_FILTER    // Commnet to Deactivate
+#define VOXEL_FILTER    // Comment to Deactivate
 #ifdef VOXEL_FILTER
 #include <pcl/filters/voxel_grid.h>
 #define LEAF_SIZE 0.03f
 #endif
-                        // Uncommnet Macro to Activate to Debugging - check the taken time for processing
+                        // Uncomment Macro to Activate Debugging - check the taken time for processing
 #define WITH_TIMING     // Comment to Deactivate
 #ifdef WITH_TIMING
 #include <chrono>
@@ -25,11 +25,33 @@
 
 ros::Publisher pub;
 
-// Distortion parameters (Intel RealSense D455)
-static double k1 = 0.00245;  // Radial distortion coefficient 0.00245
-static double k2 = -0.00037; // Radial distortion coefficient -0.00037
-static double p1 = -0.00074; // Tangential distortion coefficient -0.00074
-static double p2 = -0.00058; // Tangential distortion coefficient -0.00058
+#define UNDISTORTION
+#ifdef UNDISTORTION
+#include <sensor_msgs/point_cloud2_iterator.h>
+
+// Parameters (Intel RealSense D455)
+float k1 = 0.00245;
+float k2 = 0.0;
+float k3 = 0.0;
+float p0 = -0.00037;
+float p1 = -0.00074;
+
+void unDistortion(float& x, float& y, float& z) {
+    float r2 = x*x + y*y;
+
+    // Radial distortion
+    float radialDistort = 1 + k1 * r2 + k2 * r2 * r2 + k3 * r2 * r2 * r2;
+    x *= radialDistort;
+    y *= radialDistort;
+
+    // Tangential distortion
+    float xTangent = 2*p0*x*y + p1*(r2 + 2*x*x);
+    float yTangent = p0*(r2 + 2*y*y) + 2*p1*x*y;
+
+    x += xTangent;
+    y += yTangent;
+}
+#endif
 
 void filterPointCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr& iCloud, pcl::PointCloud<pcl::PointXYZ>::Ptr& fCloud) {
     #ifdef WITH_TIMING
@@ -39,7 +61,7 @@ void filterPointCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr& iCloud, pcl::Po
     pcl::PassThrough<pcl::PointXYZ> p;
     p.setInputCloud(iCloud);
     p.setFilterFieldName("x");
-    p.setFilterLimits(-0.5, 0.5);
+    p.setFilterLimits(-3.0, 3.0);
     p.filter(*fCloud);
 
     p.setInputCloud(iCloud);
@@ -55,43 +77,37 @@ void filterPointCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr& iCloud, pcl::Po
     #endif
     #ifdef WITH_TIMING
         auto toc = std::chrono::high_resolution_clock::now();
-
         std::cout << "[Filtering] took " << 1e-3 * std::chrono::duration_cast<std::chrono::microseconds>(toc - tic).count() << "ms" << std::endl;
     #endif
 }
 
 void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloudMsg) {
+    sensor_msgs::PointCloud2 cloud = *cloudMsg;
+
     // Convert PointCloud2 msg to PCL format
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cCloud(new pcl::PointCloud<pcl::PointXYZ>());
     pcl::PointCloud<pcl::PointXYZ>::Ptr fCloud(new pcl::PointCloud<pcl::PointXYZ>());
-    pcl::fromROSMsg(*cloudMsg, *cloud);
-    filterPointCloud(cloud, fCloud);
-    std::cout << "[Filtering] Number of points: " << fCloud->size() << std::endl;
+    pcl::fromROSMsg(cloud, *cCloud);
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr correctedCloud(new pcl::PointCloud<pcl::PointXYZ>());
+    #ifdef UNDISTORTION
+        sensor_msgs::PointCloud2Modifier modifier(cloud);
+        sensor_msgs::PointCloud2Iterator<float> xIter(cloud, "x");
+        sensor_msgs::PointCloud2Iterator<float> yIter(cloud, "y");
+        sensor_msgs::PointCloud2Iterator<float> zIter(cloud, "z");
 
-    // Apply distortion correction
-    for (const auto& point : fCloud->points) {
-        if (point.z > 0) {
-            // Compute normalized coordinates
-            double x_normalized = point.x / point.z;
-            double y_normalized = point.y / point.z;
+        for (; xIter != xIter.end(); ++xIter, ++yIter, ++zIter) {
+            float x = *xIter;
+            float y = *yIter;
+            float z = *zIter;
 
-            // Compute radial distance from the optical center
-            double r2 = x_normalized * x_normalized + y_normalized * y_normalized;
-            double radialDistortion = 1 + k1 * r2 + k2 * r2 * r2;
-
-            // Correct x and y coordinates with radial and tangential distortion
-            double x_corrected = x_normalized * radialDistortion + (2 * p1 * x_normalized * y_normalized + p2 * (r2 + 2 * x_normalized * x_normalized));
-            double y_corrected = y_normalized * radialDistortion + (p1 * (r2 + 2 * y_normalized * y_normalized) + 2 * p2 * x_normalized * y_normalized);
-
-            // Rescale corrected coordinates back to 3D
-            x_corrected *= point.z;
-            y_corrected *= point.z;
-
-            correctedCloud->points.push_back(pcl::PointXYZ(x_corrected, y_corrected, point.z));
+            // Apply distortion correction
+            unDistortion(x, y, z);
         }
-    }
+    #endif
+
+    // Filter the point cloud
+    filterPointCloud(cCloud, fCloud);
+    std::cout << "[Filtering] Number of points: " << fCloud->size() << std::endl;
 
     // Camera coordinates -> World coordinates (apply tf manually)
     static tf::TransformListener listener;
@@ -101,7 +117,7 @@ void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloudMsg) {
         listener.lookupTransform("/Neo", "/Camera_OmniVision_OV9782_Color", ros::Time(0), transform);
 
         // Apply the transform to each point manually
-        for (auto& point : correctedCloud->points) {
+        for (auto& point : fCloud->points) {
             tf::Vector3 pointCam(point.x, point.y, point.z);
             tf::Vector3 pointWorld = transform * pointCam;
             point.x = pointWorld.x();
@@ -111,7 +127,7 @@ void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloudMsg) {
 
         // Convert to PointCloud2
         sensor_msgs::PointCloud2 output;
-        pcl::toROSMsg(*correctedCloud, output);
+        pcl::toROSMsg(*fCloud, output);
         output.header.frame_id = "World";
         pub.publish(output);
 
@@ -125,10 +141,10 @@ int main(int argc, char** argv) {
     ros::NodeHandle nh;
 
     // Publish PointCloud2 topic "/trans_depth_cloud"
-    pub = nh.advertise<sensor_msgs::PointCloud2>("trans_depth_cloud", 1);
+    pub = nh.advertise<sensor_msgs::PointCloud2>("trans_depth_cloud", 10);
 
     // Subscribe the topic "/depth_cloud"
-    ros::Subscriber sub = nh.subscribe("/depth_cloud", 1, pointCloudCallback);
+    ros::Subscriber sub = nh.subscribe("/depth_cloud", 10, pointCloudCallback);
 
     ros::spin();
     return 0;
